9c4f7baa613e7ee247d562d06176fbc8ead6ed57
pantor <pantor@users.noreply.github.com>
2021-01-19 15:22:07 +0000

    deploy: 5509f7ee4e8e36b22252d4f749b1bc65e8429d36

--- search/all_14.js
+++ search/all_14.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['v',['v',['../structruckig_1_1_profile.html#a9037fdd1f5a069723d4a48a6b3b08722',1,'ruckig::Profile']]],
+  ['v_5fat_5fa_5fzero',['v_at_a_zero',['../namespaceruckig.html#a7fd5b73571b4274e4062b57ddb086714',1,'ruckig']]],
   ['v_5fat_5ft',['v_at_t',['../namespaceruckig.html#a44a56a5b90b258ac5f1118f3a91905f6',1,'ruckig']]],
   ['v_5fbrakes',['v_brakes',['../structruckig_1_1_profile.html#a1186ebb86cc607d02cc20e4e9b3edf57',1,'ruckig::Profile']]],
   ['validate_5finput',['validate_input',['../classruckig_1_1_smoothie.html#aea5beb601658796ebe001eb7ee9cf2b1',1,'ruckig::Smoothie::validate_input()'],['../classruckig_1_1_ruckig.html#ad3b2da32e4d62f44c95f05609281de3d',1,'ruckig::Ruckig::validate_input()']]],
