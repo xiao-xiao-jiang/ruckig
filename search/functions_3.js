d33742393c7fdad9444799c454c33fbb154a377d
pantor <pantor@users.noreply.github.com>
2021-01-26 09:19:01 +0000

    deploy: b4fc9294f7158a0928da597d25cd8ba297e0bb8e

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['get_5fbrake_5ftrajectory',['get_brake_trajectory',['../classruckig_1_1_brake.html#acf76cd8a84b6efaefd0b5c4b1a8b914d',1,'ruckig::Brake']]],
-  ['get_5fprofile',['get_profile',['../classruckig_1_1_step1.html#a2c322c8762f9bb07b8e5b39f15c39c2f',1,'ruckig::Step1::get_profile()'],['../classruckig_1_1_step2.html#a124815248e5fee5792d61b712ae713d9',1,'ruckig::Step2::get_profile()']]],
+  ['get_5fprofile',['get_profile',['../classruckig_1_1_step1.html#ad18be1b34895513f86d752eb3be672c0',1,'ruckig::Step1::get_profile()'],['../classruckig_1_1_step2.html#a124815248e5fee5792d61b712ae713d9',1,'ruckig::Step2::get_profile()']]],
   ['get_5ftime',['get_time',['../classruckig_1_1_ruckig.html#a40c42e58f50131d80ee01f3b770a4b49',1,'ruckig::Ruckig']]]
 ];
--- search/variables_1.js
+++ search/variables_1.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['b',['b',['../structruckig_1_1_block.html#aa7705929da48ef8620b7e2afd55a4ecf',1,'ruckig::Block']]],
-  ['block',['block',['../classruckig_1_1_step1.html#ad7371158f4ab453a74e332075da88c46',1,'ruckig::Step1']]]
+  ['b',['b',['../structruckig_1_1_block.html#aa7705929da48ef8620b7e2afd55a4ecf',1,'ruckig::Block']]]
 ];
