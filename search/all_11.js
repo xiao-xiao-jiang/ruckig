62bce3da1e4d742db57882aa0f22156078853002
pantor <pantor@users.noreply.github.com>
2021-02-09 17:54:31 +0000

    deploy: d0ad9f8bd32b732656fcd55668975da97c581395

--- search/all_11.js
+++ search/all_11.js
@@ -8,9 +8,9 @@ var searchData=
   ['solvequartmonic',['solveQuartMonic',['../namespace_roots.html#a5765290107032cd45feb1735845032e3',1,'Roots::solveQuartMonic(double a, double b, double c, double d)'],['../namespace_roots.html#a46343db1e513e5d7ffe22eadb17ffb2c',1,'Roots::solveQuartMonic(const std::array&lt; double, 5 &gt; &amp;polynom)']]],
   ['solveresolvent',['solveResolvent',['../namespace_roots.html#a667459f4b47b151b3114ed626c11d491',1,'Roots']]],
   ['sqrt',['Sqrt',['../namespaceruckig.html#a33eb514e2c5ef12e59e6dd0e1bfa6628',1,'ruckig']]],
-  ['step1',['Step1',['../classruckig_1_1_step1.html',1,'ruckig::Step1'],['../classruckig_1_1_step1.html#a8b7112ef296ebb2869824f04c9d63bcf',1,'ruckig::Step1::Step1()']]],
+  ['step1',['Step1',['../classruckig_1_1_step1.html',1,'ruckig::Step1'],['../classruckig_1_1_step1.html#a6bfcc93b07668bbc0df845948c86d1ea',1,'ruckig::Step1::Step1()']]],
   ['step1_2ecpp',['step1.cpp',['../step1_8cpp.html',1,'']]],
-  ['step2',['Step2',['../classruckig_1_1_step2.html',1,'ruckig::Step2'],['../classruckig_1_1_step2.html#a8ad35bb1ec4a168842b335a6976f2d19',1,'ruckig::Step2::Step2()']]],
+  ['step2',['Step2',['../classruckig_1_1_step2.html',1,'ruckig::Step2'],['../classruckig_1_1_step2.html#abb20f7f1412e9b21d9a80165f2ef4a0a',1,'ruckig::Step2::Step2()']]],
   ['step2_2ecpp',['step2.cpp',['../step2_8cpp.html',1,'']]],
   ['steps_2ehpp',['steps.hpp',['../steps_8hpp.html',1,'']]]
 ];
--- search/variables_8.js
+++ search/variables_8.js
@@ -5,6 +5,7 @@ var searchData=
   ['max_5fjerk',['max_jerk',['../classruckig_1_1_input_parameter.html#a8c61040f44fad0849ebb313979a3cd44',1,'ruckig::InputParameter']]],
   ['max_5fvelocity',['max_velocity',['../classruckig_1_1_input_parameter.html#a76612facb7c6897da86d9a18304ef46f',1,'ruckig::InputParameter']]],
   ['min',['min',['../structruckig_1_1_position_extrema.html#a73f34a1fd1bc66bc3ace575c86616b98',1,'ruckig::PositionExtrema']]],
+  ['min_5facceleration',['min_acceleration',['../classruckig_1_1_input_parameter.html#aaa49014ab71cc6e0bb7f6b918b91bdd3',1,'ruckig::InputParameter']]],
   ['min_5fvelocity',['min_velocity',['../classruckig_1_1_input_parameter.html#ab46edaeca9a9d991b2e7c7ffad6c9119',1,'ruckig::InputParameter']]],
   ['minimum_5fduration',['minimum_duration',['../classruckig_1_1_input_parameter.html#a9ff807bf36d58afc6648150dbb78d4a6',1,'ruckig::InputParameter']]]
 ];
