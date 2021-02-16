78909348d17452bca8b92e8cca372b5cc1ff4f2d
pantor <pantor@users.noreply.github.com>
2021-02-16 08:38:39 +0000

    deploy: edce4a8bfe93e8de70a37064a69bd6418dfcc1d5

--- search/enumvalues_6.js
+++ search/enumvalues_6.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['timealways',['TimeAlways',['../classruckig_1_1_input_parameter.html#a1188a44f84ed73ddb255abb12daf252da90f6ad7a6c79fcdbccfa0c748573c8c8',1,'ruckig::InputParameter']]],
+  ['time',['Time',['../classruckig_1_1_input_parameter.html#a1188a44f84ed73ddb255abb12daf252daa76d4ef5f3f6a672bbfab2865563e530',1,'ruckig::InputParameter']]],
   ['timeifnecessary',['TimeIfNecessary',['../classruckig_1_1_input_parameter.html#a1188a44f84ed73ddb255abb12daf252da7ddc8e24add04a76f06998f3f12404e2',1,'ruckig::InputParameter']]]
 ];
--- search/variables_c.js
+++ search/variables_c.js
@@ -9,8 +9,7 @@ var searchData=
   ['target_5facceleration',['target_acceleration',['../classruckig_1_1_input_parameter.html#a0a27ad818be39d08b0c04dd16110eb4f',1,'ruckig::InputParameter']]],
   ['target_5fposition',['target_position',['../classruckig_1_1_input_parameter.html#afdaa639547877a33b55db961c61a2a5b',1,'ruckig::InputParameter']]],
   ['target_5fvelocity',['target_velocity',['../classruckig_1_1_input_parameter.html#ac63e509adce3b0da1de847ee79cf228d',1,'ruckig::InputParameter']]],
-  ['time',['time',['../structruckig_1_1_output_parameter.html#ae5d91694171325f0a87eca37d90cc27a',1,'ruckig::OutputParameter']]],
-  ['timealways',['TimeAlways',['../classruckig_1_1_input_parameter.html#a202f47103ec21fa967c5f7d2d8b5b1c0',1,'ruckig::InputParameter']]],
+  ['time',['Time',['../classruckig_1_1_input_parameter.html#a7b6f9cf12ac16cbee522550bf795ea2c',1,'ruckig::InputParameter::Time()'],['../structruckig_1_1_output_parameter.html#ae5d91694171325f0a87eca37d90cc27a',1,'ruckig::OutputParameter::time()']]],
   ['tolerance',['tolerance',['../namespace_roots.html#ac8574d616e7281fb23b50d0611a1d5f0',1,'Roots']]],
   ['trajectory',['trajectory',['../structruckig_1_1_output_parameter.html#a792e57830acbd01e894982b5e5bcd885',1,'ruckig::OutputParameter']]]
 ];
