5ad3ff469eafed745025be659d2c237370a5340c
pantor <pantor@users.noreply.github.com>
2021-04-12 09:54:18 +0000

    deploy: 8165fae101f874c3ce73d765c68d02c1416a162d

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['calculate_223',['calculate',['../classruckig_1_1_trajectory.html#a25f1640ca53d093dd88749cb240bd573',1,'ruckig::Trajectory']]],
-  ['calculate_5fblock_224',['calculate_block',['../classruckig_1_1_block.html#a834329fe8e6f33f28fdca1c1b04e1161',1,'ruckig::Block']]],
+  ['calculate_5fblock_224',['calculate_block',['../classruckig_1_1_block.html#a276cb1d05d95ac274486e4cd57ca2bc6',1,'ruckig::Block']]],
   ['cast_5funique_225',['cast_unique',['../python_8cpp.html#a611c9e2fa01f6a49ca49c43542f5b925',1,'cast_unique():&#160;python.cpp'],['../python_8cpp.html#a49c3476341e9a56591ae7eab27ec6f19',1,'cast_unique(double delta_time):&#160;python.cpp']]],
   ['check_226',['check',['../structruckig_1_1_profile.html#afe24e5ffae0b039e9345a62c52510be7',1,'ruckig::Profile::check(double jf, double aMax, double aMin)'],['../structruckig_1_1_profile.html#a982cc20d0fefce298992c9d92bc2815c',1,'ruckig::Profile::check(double jf, double vMax, double aMax, double aMin)'],['../structruckig_1_1_profile.html#a215a6d742190d89a91bee6e95981cc74',1,'ruckig::Profile::check([[maybe_unused]] double tf, double jf, double vMax, double aMax, double aMin)'],['../structruckig_1_1_profile.html#ad4335b4b56a5ee9c5a260f2fdd2f01cb',1,'ruckig::Profile::check(double tf, double jf, double vMax, double aMax, double aMin, double jMax)']]],
   ['check_5fposition_5fextremum_227',['check_position_extremum',['../structruckig_1_1_profile.html#a3917ca8e3fc834a0f811646354014143',1,'ruckig::Profile']]],
